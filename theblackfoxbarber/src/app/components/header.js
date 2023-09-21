290021cf1f69b3f407c93e5b66f5f92246eab91e
Théo Daudebourg <54123008+LilZloty@users.noreply.github.com>
2023-09-20 22:55:25 -0600

    push about page

---
+++ theblackfoxbarber/src/app/components/header.js
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+import Image from 'next/image'
+import AboutPage from '../pages/about.js';
+
+const Header = () => {
+  return (
+    <header className="bg-gray-800 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="logo text-2xl font-bold"> <Image
+            src="/images/theblackfoxbarberlogo.png"
+            alt="The black fox barber logo"
+            width={180}
+            height={180}
+          /></div>
+        <nav className="space-x-4">
+          <Link href="/">
+            Home
+          </Link>
+          <Link href="/about">
+            About
+          </Link>
+          <Link href="/services">
+            Services
+          </Link>
+          <Link href="/contact">
+            Contact
+          </Link>
+        </nav>
+        <div className="contact-info">Contact Info</div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ theblackfoxbarber/src/app/pages/about.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+import Image from 'next/image'
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This is the About Us page content.</p>
+    </div>
+  );
+};
+
+export default AboutPage;
