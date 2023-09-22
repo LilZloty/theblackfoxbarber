5a4d85e0b1b4b0119c5dedf974f275970586e9bc
Théo Daudebourg <54123008+LilZloty@users.noreply.github.com>
2023-09-21 23:25:20 -0600

    Update header.js

--- theblackfoxbarber/src/app/components/header.js
+++ theblackfoxbarber/src/app/components/header.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 const Header = () => {
   return (
-    <header className="bg-gray-800 text-white p-4">
+    <header className="bg-transparent text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
         <div className="logo text-2xl font-bold"> <Image
             src="/images/theblackfoxbarberlogo.png"
